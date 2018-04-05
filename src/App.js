441e30500442048e37afe9380517f1f54051e33b
Amanda Harlin <amanda.n.harlin@gmail.com>
2018-04-05 11:15:08 -0500

    mocking out some html to start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,103 @@
+import 'semantic-ui-css/semantic.min.css';
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Checkbox,
+  Container,
+  Divider,
+  Icon,
+  Input,
+  Table
+} from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Divider hidden />
+          <Table collapsing celled compact striped color="teal">
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Finished?</Table.HeaderCell>
+                <Table.HeaderCell>Chore</Table.HeaderCell>
+                <Table.HeaderCell>Points</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox label="" />
+                </Table.Cell>
+                <Table.Cell>mop floors</Table.Cell>
+                <Table.Cell>1</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox label="" />
+                </Table.Cell>
+                <Table.Cell>roll down trash</Table.Cell>
+                <Table.Cell>5</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox label="" />
+                </Table.Cell>
+                <Table.Cell>take out trash</Table.Cell>
+                <Table.Cell>3</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+          <Input
+            icon={{ name: 'plus  ', circular: true, link: true }}
+            placeholder="Add new chore"
+          />
+
+          <Divider hidden />
+          <Table collapsing celled compact striped color="pink">
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Finished?</Table.HeaderCell>
+                <Table.HeaderCell>Chore</Table.HeaderCell>
+                <Table.HeaderCell>Points</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {/* <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox label="" />
+                </Table.Cell>
+                <Table.Cell>mop floors</Table.Cell>
+                <Table.Cell>1</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox label="" />
+                </Table.Cell>
+                <Table.Cell>roll down trash</Table.Cell>
+                <Table.Cell>5</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox label="" />
+                </Table.Cell>
+                <Table.Cell>take out trash</Table.Cell>
+                <Table.Cell>3</Table.Cell>
+              </Table.Row> */}
+
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox label="" />
+                </Table.Cell>
+                <Table.Cell />
+                <Table.Cell />
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Container>
       </div>
     );
   }
